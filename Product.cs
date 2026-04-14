07e09600e3577fd4f2d560120afcac09e9d14ca0
martinez-marithe <marithecm1028@gmail.com>
2026-04-15 01:56:25 +0800

    fix permissions

---
+++ Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Martinez_Marithe_ShoppingCartActivity
+{
+    internal class Product
+    {
+        //anotara
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Martinez_Marithe_ShoppingCartActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

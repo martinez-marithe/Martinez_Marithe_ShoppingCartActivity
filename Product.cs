d1b7b9c57ac3f7bbe32c895be32f63971c35fda2
martinez-marithe <marithecm1028@gmail.com>
2026-04-15 11:06:01 +0800

    Initialized skincare store array and implemented menu display loop

--- Product.cs
+++ Product.cs
@@ -17,8 +17,18 @@ class Program
 {
     static void main()
     {
-        Product p1 = new Product { Id = 1, Name = "Mineral sunscreen", Price = 1200, RemainingStock = 10 };
-        p1.DisplayProduct();
+        Product[] store = new Product[5];
+        store[0] = new Product { Id = 1, Name = "Mineral Sunscreen", Price = 1200, RemainingStock = 10 };
+        store[1] = new Product { Id = 2, Name = "AHA/BHA Exfoliant Set", Price = 5500, RemainingStock = 3 };
+        store[2] = new Product { Id = 3, Name = "Vitamin C Serum", Price = 1900, RemainingStock = 7 };
+        store[3] = new Product { Id = 4, Name = "Cleansing Oil", Price = 800, RemainingStock = 12 };
+        store[4] = new Product { Id = 5, Name = "Gentle Facial Cleanser", Price = 560, RemainingStock = 15 };
+
+        Console.WriteLine("=== GLOW & CARE SKINCARE STORE ===");
+        for (int i = 0; i < store.Length; i++)
+        {
+            store[i].DisplayProduct();
+        }
         Console.ReadKey();
     }
 }

e13811f531e61f2095fab3cde6761c2797b35bb0
martinez-marithe <marithecm1028@gmail.com>
2026-04-15 09:54:27 +0800

    Setup Product class

--- Product.cs
+++ Product.cs
@@ -17,3 +17,20 @@ public class Product
         return Price * quantity;
     }
 }
+
+class Program
+{
+    static void main()
+    {
+        Product p1 = new Product();
+        p1.Id = 1;
+        p1.Name = "Mineral Sunscreen";
+        p1.Price = 1200;
+        p1.RemainingStock = 5;
+
+        p1.DisplayProduct();
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void main()
-    {
-        Product p1 = new Product();
-        p1.Id = 1;
-        p1.Name = "Mineral Sunscreen";
-        p1.Price = 1200;
-        p1.RemainingStock = 5;
-
-        p1.DisplayProduct();
-
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
-    }
-}
+﻿//empty

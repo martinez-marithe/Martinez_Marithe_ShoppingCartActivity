122d44f5cf84490da6f92ffa6569e8c82dc28ae7
martinez-marithe <marithecm1028@gmail.com>
2026-05-10 22:12:43 +0800

    add SkincareProduct class

--- SkincareProduct.cs
+++ SkincareProduct.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Martinez_Marithe_ShoppingCartActivity
+public class SkincareProduct : Product
 {
-    internal class SkincareProduct
+    private int spfLevel;
+
+    public SkincareProduct(int id , string name, double price, int remainingStock, string category, int spfLevel)
+        : base(id, name, price, remainingStock, category)
     {
+        this.spfLevel = spfLevel;
+    }
+
+    public int GetSpfLevel()
+    {
+        return spfLevel;
+    }
+
+    public void SetSpfLevel(int spfLevel)
+    {
+        this.spfLevel = spfLevel;
+    }
+
+    public void DisplaySkincareProduct()
+    {
+        Console.WriteLine(GetId() + ". [" + GetCategory() + "] " + GetName() + " - P" + GetPrice() + " (Stock: " + GetremainingStock() + ") SPF: " + spfLevel);
     }
 }
